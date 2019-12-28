0bc125c9d394e22d11b15b6429fd4e894a6866d8
Artoria Salvatore <35622087+tickcount@users.noreply.github.com>
2019-12-28 15:53:55 +0300

    Create a_wasd_strafer_fix.lua

---
+++ a_wasd_strafer_fix.lua
@@ -0,0 +1,36 @@
+local strafer = ui.reference('MISC', 'Movement', 'Air strafe')
+local strafer_direction = ui.reference('MISC', 'Movement', 'Air strafe direction')
+local strafer_smoothing = ui.reference('MISC', 'Movement', 'Air strafe smoothing')
+
+local contains = function(tab, val) for i = 1, #tab do if tab[i] == val then return true; end end; return false end
+local clamp = function(val, min, max) local value = val; value = value < min and min or value; value = value > max and max or value; return value end
+
+local command = function(e)
+    if not ui.get(strafer) or not contains(ui.get(strafer_direction), 'Movement keys') or e.in_jump == 0 then
+        ui.set(strafer_smoothing, 0)
+        return
+    end
+
+    local list = {
+        e.in_forward, 
+        e.in_back,
+        e.in_moveleft,
+        e.in_moveright
+    }
+
+    if contains(list, 1) then
+        local air_accel = cvar.sv_airaccelerate:get_int()
+        local end_val = -0.614*air_accel + 686/11
+
+        ui.set(strafer_smoothing, clamp(end_val, 1, 99))
+    else
+        ui.set(strafer_smoothing, 0)
+    end
+end
+
+ui.set_visible(strafer_smoothing, false)
+
+client.set_event_callback('setup_command', command)
+client.set_event_callback('shutdown', function()
+    ui.set_visible(strafer_smoothing, true)
+end)
