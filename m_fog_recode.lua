0c2ae9c1542c1b44cf2fb8eafb352f75a8220e02
tickcount <35622087+tickcount@users.noreply.github.com>
2020-07-29 17:03:41 +0300

    Update m_fog_recode.lua

--- m_fog_recode.lua
+++ m_fog_recode.lua
@@ -8,6 +8,7 @@ local fog = {
 local dir = { 'VISUALS', 'Effects' }
 local active = ui.new_checkbox(dir[1], dir[2], 'FOG Correction')
 local color = ui.new_color_picker(dir[1], dir[2], 'FOG Correction color', 52, 57, 71, 0)
+local start_distance = ui.new_slider(dir[1], dir[2], 'FOG Start Distance', 0, 2500, 500)
 local distance = ui.new_slider(dir[1], dir[2], 'FOG Distance', 0, 2500, 1420)
 local density = ui.new_slider(dir[1], dir[2], 'FOG Density', 0, 100, 70)
 
@@ -19,7 +20,7 @@ local g_paint = function()
 
     client.set_cvar('fog_override', 1)
 
-    fog.start:set_int(0)
+    fog.start:set_int(ui.get(start_distance))
     fog.send:set_int(ui.get(distance))
     fog.maxdensity:set_float(ui.get(density)/100)
     fog.color:set_string(string.format('%s %s %s', ui.get(color)))
