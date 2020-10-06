71b68cb5de6a4890b4895c542a0b3bfad1b161ad
tickcount <35622087+tickcount@users.noreply.github.com>
2020-10-06 11:39:03 +0300

    Update c_custom_scope.lua

--- c_custom_scope.lua
+++ c_custom_scope.lua
@@ -13,24 +13,24 @@ local overlay_offset = ui_new_slider('Visuals', 'Effects', '\n scope_lines_offse
 local fade_time = ui_new_slider('Visuals', 'Effects', 'Fade animation speed', 3, 20, 12, true, 'fr', 1, { [3] = 'Off' })
 
 local g_paint_ui = function()
-	ui_set(scope_overlay, true)
+    ui_set(scope_overlay, true)
 end
 
 local g_paint = function()
     ui_set(scope_overlay, false)
 
     local width, height = client_screen_size()
-	local offset, initial_position, speed, color =
+    local offset, initial_position, speed, color =
         ui_get(overlay_offset) * height / 1080, 
         ui_get(overlay_position) * height / 1080, 
         ui_get(fade_time), { ui_get(color_picker) }
 
-	local me = entity_get_local_player()
-	local wpn = entity_get_player_weapon(me)
+    local me = entity_get_local_player()
+    local wpn = entity_get_player_weapon(me)
 
-	local scope_level = entity_get_prop(wpn, 'm_zoomLevel')
-	local scoped = entity_get_prop(me, 'm_bIsScoped') == 1
-	local resume_zoom = entity_get_prop(me, 'm_bResumeZoom') == 1
+    local scope_level = entity_get_prop(wpn, 'm_zoomLevel')
+    local scoped = entity_get_prop(me, 'm_bIsScoped') == 1
+    local resume_zoom = entity_get_prop(me, 'm_bResumeZoom') == 1
 
     local is_valid = entity_is_alive(me) and wpn ~= nil and scope_level ~= nil
     local act = is_valid and scope_level > 0 and scoped and not resume_zoom
@@ -38,10 +38,10 @@ local g_paint = function()
     local FT = speed > 3 and globals_frametime() * speed or 1
     local alpha = easing.linear(m_alpha, 0, 1, 1)
 
-	renderer_gradient(width/2 - initial_position, height / 2, initial_position - offset, 1, color[1], color[2], color[3], 0, color[1], color[2], color[3], alpha*color[4], true)
-	renderer_gradient(width/2 + offset, height / 2, initial_position - offset, 1, color[1], color[2], color[3], alpha*color[4], color[1], color[2], color[3], 0, true)
+    renderer_gradient(width/2 - initial_position, height / 2, initial_position - offset, 1, color[1], color[2], color[3], 0, color[1], color[2], color[3], alpha*color[4], true)
+    renderer_gradient(width/2 + offset, height / 2, initial_position - offset, 1, color[1], color[2], color[3], alpha*color[4], color[1], color[2], color[3], 0, true)
 
-	renderer_gradient(width / 2, height/2 - initial_position, 1, initial_position - offset, color[1], color[2], color[3], 0, color[1], color[2], color[3], alpha*color[4], false)
+    renderer_gradient(width / 2, height/2 - initial_position, 1, initial_position - offset, color[1], color[2], color[3], 0, color[1], color[2], color[3], alpha*color[4], false)
     renderer_gradient(width / 2, height/2 + offset, 1, initial_position - offset, color[1], color[2], color[3], alpha*color[4], color[1], color[2], color[3], 0, false)
     
     m_alpha = clamp(m_alpha + (act and FT or -FT), 0, 1)
@@ -50,8 +50,8 @@ end
 local ui_callback = function(c)
     local master_switch, addr = ui_get(c), ''
 
-	if not master_switch then
-		m_alpha, addr = 0, 'un'
+    if not master_switch then
+        m_alpha, addr = 0, 'un'
     end
     
     local _func = client[addr .. 'set_event_callback']
@@ -61,8 +61,8 @@ local ui_callback = function(c)
     ui_set_visible(overlay_offset, master_switch)
     ui_set_visible(fade_time, master_switch)
 
-	_func('paint_ui', g_paint_ui)
-	_func('paint', g_paint)
+    _func('paint_ui', g_paint_ui)
+    _func('paint', g_paint)
 end
 
 ui_set_callback(master_switch, ui_callback)
