a1f843cd15bf409c15622c81af8206b87abe2d76
tickcount <35622087+tickcount@users.noreply.github.com>
2020-12-19 12:48:51 +0300

    Added Viewmodel Roll

--- o_viewmodel.lua
+++ o_viewmodel.lua
@@ -1,6 +1,8 @@
-local client_set_event_callback, entity_get_classname, entity_get_local_player, entity_get_player_weapon, string_match, ui_get, ui_new_combobox, ui_new_slider = client.set_event_callback, entity.get_classname, entity.get_local_player, entity.get_player_weapon, string.match, ui.get, ui.new_combobox, ui.new_slider
+local client_camera_angles, client_create_interface, client_find_signature, client_set_event_callback, entity_get_classname, entity_get_local_player, entity_get_player_weapon, entity_get_prop, string_match, ui_get, error, ui_new_combobox, ui_new_slider = client.camera_angles, client.create_interface, client.find_signature, client.set_event_callback, entity.get_classname, entity.get_local_player, entity.get_player_weapon, entity.get_prop, string.match, ui.get, error, ui.new_combobox, ui.new_slider
+local get_cvar, vo_hand, vfov, vo_x, vo_y, vo_z = client.get_cvar, cvar.cl_righthand, cvar.viewmodel_fov, cvar.viewmodel_offset_x, cvar.viewmodel_offset_y, cvar.viewmodel_offset_z
 
 local dir, restore = { 'LUA', 'B', 4000, { '-', 'Left hand', 'Right hand' } }
+
 local menu = {
     kpos = ui_new_combobox(dir[1], dir[2], 'Knife positioning', dir[4]),
     fov = ui_new_slider(dir[1], dir[2], 'Viewmodel FOV', -dir[3], dir[3], 0, true, '', 0.01),
@@ -8,14 +10,27 @@ local menu = {
     x = ui_new_slider(dir[1], dir[2], 'Viewmodel offset X', -dir[3], dir[3], 0, true, '', 0.01),
     y = ui_new_slider(dir[1], dir[2], 'Viewmodel offset Y', -dir[3], dir[3], 0, true, '', 0.01),
     z = ui_new_slider(dir[1], dir[2], 'Viewmodel offset Z', -dir[3], dir[3], 0, true, '', 0.01),
+    roll = ui_new_slider(dir[1], dir[2], 'Viewmodel offset Roll', -180, 180, 0, true),
 }
 
-local get_cvar = client.get_cvar
-local vfov = cvar.viewmodel_fov
-local vo_x = cvar.viewmodel_offset_x
-local vo_y = cvar.viewmodel_offset_y     
-local vo_z = cvar.viewmodel_offset_z
-local vo_hand = cvar.cl_righthand
+local ffi, bit = require 'ffi', require 'bit'
+local ffi_to = {
+    classptr = ffi.typeof('void***'), 
+    client_entity = ffi.typeof('void*(__thiscall*)(void*, int)'),
+    
+    set_angles = (function()
+        ffi.cdef('typedef struct { float x; float y; float z; } vec3_t;')
+
+        return ffi.typeof('void(__thiscall*)(void*, const vec3_t&)')
+    end)()
+}
+
+local rawelist = client_create_interface('client_panorama.dll', 'VClientEntityList003') or error('VClientEntityList003 is nil', 2)
+local ientitylist = ffi.cast(ffi_to.classptr, rawelist) or error('ientitylist is nil', 2)
+local get_client_entity = ffi.cast(ffi_to.client_entity, ientitylist[0][3]) or error('get_client_entity is nil', 2)
+
+local set_angles = client_find_signature('client_panorama.dll', '\x55\x8B\xEC\x83\xE4\xF8\x83\xEC\x64\x53\x56\x57\x8B\xF1') or error('Couldn\'t find set_angles signature!')
+local set_angles_fn = ffi.cast(ffi_to.set_angles, set_angles) or error('Couldn\'t cast set_angles_fn')
 
 local get_original = function()
     return {
@@ -30,15 +45,15 @@ end
 
 local g_handler = function(...)
     local shutdown = #({...}) > 0
+
     local multiplier = shutdown and 0 or 0.0025
-    
     local original, data = get_original(), 
     {
         rhand = ui_get(menu.kpos),
         fov = ui_get(menu.fov) * multiplier,
         x = ui_get(menu.x) * multiplier,
         y = ui_get(menu.y) * multiplier,
-        z = ui_get(menu.z) * multiplier,
+        z = ui_get(menu.z) * multiplier
     }
 
     vfov:set_raw_float(original.fov + data.fov)
@@ -66,5 +81,34 @@ local g_handler = function(...)
     end
 end
 
-client_set_event_callback('shutdown', function() g_handler(true) end)
+local g_override_view = function()
+    --[[
+        Credits: 
+        * tank (rave1337): proper viewmodel roll
+    ]]
+
+    local me = entity_get_local_player()
+    local viewmodel = entity_get_prop(me, 'm_hViewModel[0]')
+
+    if me == nil or viewmodel == nil then
+        return
+    end
+
+    local viewmodel_ent = get_client_entity(ientitylist, viewmodel)
+
+    if viewmodel_ent == nil then
+        return
+    end
+
+    local camera_angles = { client_camera_angles() }
+    local angles = ffi.cast('vec3_t*', ffi.new('char[?]', ffi.sizeof('vec3_t')))
+
+    angles.x, angles.y, angles.z = 
+        camera_angles[1], camera_angles[2], ui_get(menu.roll)
+
+    set_angles_fn(viewmodel_ent, angles)
+end
+
 client_set_event_callback('pre_render', g_handler)
+client_set_event_callback('override_view', g_override_view)
+client_set_event_callback('shutdown', function() g_handler(true) end)
